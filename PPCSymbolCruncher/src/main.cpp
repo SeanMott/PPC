8375a890916d87f0fb1d24fe8a26dd4f53a83a0a
Sean Mott <59934015+SeanMott@users.noreply.github.com>
2025-01-17 13:19:23 -0500

    updated argument help list

--- PPCSymbolCruncher/src/main.cpp
+++ PPCSymbolCruncher/src/main.cpp
@@ -124,7 +124,8 @@ struct ArgumentSettings
 //takes a DTK Symbol File
 #define PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_SYMBOLS "-symbols"
 
-//takes a Map file from Ghirdra or other usual decomps
+//takes a Code Warror Map and parses it out
+#define PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_CODE_WARROR_MAP "-codeWarriorMap"
 
 //takes a existing PPC Map
 #define PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_PPC_MAP "-map"
@@ -137,6 +138,43 @@ struct ArgumentSettings
 
 //sets if the PPC Map should be formated for human readability or not
 #define PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_IS_PRETTY "-pretty"
+
+//takes all the Objects and changes them to their name. This is not reccomanded as a first pass when converting DTK.
+#define PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_TYPE_OBJ_NAME "-generateTypeNames"
+
+//-----ARGUMENT SUPPORT MACROS AND FUNCTIONS---------//
+
+//defines a info entry for a command
+#define PPC_SYMBOL_CRUNCHER_PRINT_FORMAT(cmd, valueType, requiredOrNah, desc) \
+fmt::print("{} <{}> || {} || {}\n\n", cmd, valueType, requiredOrNah, desc);
+
+//prints the help screen
+static inline void PrintArgumentHelpScreen()
+{
+	PPC_SYMBOL_CRUNCHER_PRINT_FORMAT(PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_SYMBOLS, "Full File Path", "Required",
+		"Full path to the DTK generated Symbol File.")
+
+		PPC_SYMBOL_CRUNCHER_PRINT_FORMAT(PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_PPC_MAP, "Full File Path", "Optional",
+		"Full path to a existing PPC Map. If symbols is also passed in. "
+		"It will execute instead. So either " PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_SYMBOLS " or " PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_PPC_MAP " should be used ALONE to not cause conflicts.")
+
+	PPC_SYMBOL_CRUNCHER_PRINT_FORMAT(PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_OUTPUT_DIR, "Directory Full Path", "Required",
+		"Full path to the directory you would like the PPC Map outputted to.")
+
+	PPC_SYMBOL_CRUNCHER_PRINT_FORMAT(PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_MAP_NAME, "Name String", "Optional", "Name of the PPC Map file outputted. Defaults to \"Map\".")
+
+	PPC_SYMBOL_CRUNCHER_PRINT_FORMAT(PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_IS_PRETTY, "NONE", "Optional", "Makes the map output in a more human friendly way of reading, defaults to off.")
+
+	PPC_SYMBOL_CRUNCHER_PRINT_FORMAT(PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_TYPE_OBJ_NAME, "NONE", "Optional", 
+		"When this flag is passed in, all Object Symbols will have their Datatype counted in a running total. Ie if there are 12 \"4Byte\"s and 3 \"String\"s and so forth. "
+		"They will all be tracked in a global count. Once every Datatype is counted, a second pass of the Symbols will be performed. "
+		"If the Object Symbol starts with \"lbl_\", it will have it's name changed to specific it's type. For example thoses 12 Symbols with \"4Byte\" as their Datatype. "
+		"Will have their name changed to \"LBL_OBJECT_4BYTE_XXXX\". Where XXXX is the current, going from 1 to 12.\n\n"
+		"This flag is not recommended to be passed when first converting DTK Symbols and Splits into a PPC Map. "
+		"This flag is meant for use AFTER DTK Symbols have been converted into a PPC Map. AND the PPC Lexer has generated the inital Token Streams from the ASM. "
+		"Once those previous steps are done, this flag can be used to make reading Object's easier. As the Token Streams store all Identifiers via a ID. "
+		"Meaning the PPC Maps can have names freely changed without causing issues with builds.")
+}
 
 //prints that there was a error with a argument
 static inline void PrintArgumentError(const std::string& argument, const std::string& desc)
@@ -214,20 +252,7 @@ int main(int args, const char* argv[])
 
 		fmt::print("----ARGUMENT LIST----\n\n");
 		
-		fmt::print("{} <File Full Path> || Required || Full path to the symbol file.\n\n", PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_SYMBOLS);
-
-		fmt::print("{} <File Full Path> || Optional || Full path to a existing PPC Map. If symbols is also passed in. "
-			"This argument will also execute, using the current settings. So either -symbols or -map should be used to not cause conflicts.\n\n",
-			PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_PPC_MAP);
-
-		fmt::print("{} <Directory Full Path> || Required || Full path to the directory you would like the PPC Map outputted to.\n\n",
-			PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_OUTPUT_DIR);
-
-		fmt::print("{} <Name String> || Optional || Name of the PPC Map file outputted. Defaults to \"Map\".\n\n",
-			PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_MAP_NAME);
-
-		fmt::print("{} || Optional || Makes the map output in a more human friendly way of reading, defaults to off.\n\n",
-			PPC_SYMBOL_CRUNCHER_ARGUMENT_STR_IS_PRETTY);
+		PrintArgumentHelpScreen(); //prints the list of commands
 
 		getchar();
 		return 0;
@@ -236,23 +261,36 @@ int main(int args, const char* argv[])
 	//parse the arguments
 	ArgumentSettings settings = ParseArguments(args, argv);
 
-	//loads the DTK symbol file
-	std::ifstream file(settings.DTKSymbolFile);
-	std::vector<std::string> lines;
-	std::string line;
-	while (std::getline(file, line))
-		lines.emplace_back(line);
+	//symbols we generate or load
+	std::vector<PPC::SymbolMap::PPCSymbol> symbols;
 
-	//process
-	const size_t lineCount = lines.size();
-	std::vector<PPC::SymbolMap::PPCSymbol> symbols; symbols.resize(lineCount);
-	for (size_t i = 0; i < lineCount; ++i)
-		ParseDTKSymbolStringInfo(lines[i], symbols[i]);
+	//if it's a DTK Map
+	if (!settings.DTKSymbolFile.empty())
+	{
+		//loads the DTK symbol file
+		std::ifstream file(settings.DTKSymbolFile);
+		std::vector<std::string> lines;
+		std::string line;
+		while (std::getline(file, line))
+			lines.emplace_back(line);
 
+		//process
+		const size_t lineCount = lines.size();
+		symbols.clear(); symbols.resize(lineCount);
+		for (size_t i = 0; i < lineCount; ++i)
+			ParseDTKSymbolStringInfo(lines[i], symbols[i]);
+	}
+
+	//if it's a existing PPC Map
+	else if (!settings.existingPPCMapFile.empty())
+		symbols = PPC::SymbolMap::LoadPPCSymbolMap(settings.existingPPCMapFile);
+
+	//if it's a Code Warror Map
+	
 	//generate the .ppcmap file
 	const std::filesystem::path ppcMap = std::filesystem::path(settings.PPCMapOutputDir.string() + "/" + settings.mapName + PPC_MAP_FILE_EXTENSION);
 	PPC::SymbolMap::DumpPPCSymbolsToMap(ppcMap, symbols, settings.isPretty);
-	
+
 	//getchar();
 	return 0;
 }
